418192a372c67dd2e6650c6d513e591e13aff335
facundo loureiro <faculoureiro99@gmail.com>
2024-07-04 21:29:37 -0300

    Initial commit

---
+++ Generador QR/Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿using MudBlazor;
+
+
+namespace Generador_QR.Pages
+{
+    public partial class Home
+    {
+        MudForm urlSubmitForm;
+        public string SubmittedUrl { get; set; }
+        public string QRCodeText { get; set; }
+    }
+}
