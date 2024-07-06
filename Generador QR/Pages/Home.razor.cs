daa7e87a2cf7a9a22c38ceacbdd21b0e90074ec2
facundo loureiro <faculoureiro99@gmail.com>
2024-07-05 23:52:34 -0300

    QRCode

--- Generador QR/Pages/Home.razor.cs
+++ Generador QR/Pages/Home.razor.cs
@@ -1,4 +1,5 @@
 ﻿using MudBlazor;
+using QRCoder;
 
 
 namespace Generador_QR.Pages
@@ -8,5 +9,25 @@ namespace Generador_QR.Pages
         MudForm urlSubmitForm;
         public string SubmittedUrl { get; set; }
         public string QRCodeText { get; set; }
+
+        private async Task SubmittedUrl()
+        {
+            async urlSubmitForm.Validate();
+            if(urlSubmitForm.IsValid)
+                GenerateQRCode();
+        }
+
+
+        protected void GenerateQRCode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(SubmittedUrl, QRCodeGenerator.ECCLevel.Q);
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qRCodeData);
+            byte[] qrCodeAsByteArr = qrCode.GetGraphic(20);
+
+            var ms = new MemoryStream(qrCodeAsByteArr);
+            QRCodeText = "data:/image/png;base64," + Convert.ToBase64String(ms.ToArray());
+            SubmittedUrl = String.Empty;
+        }
     }
 }
